b6926503d329905e034a1ef092cc6eb88cca194b
Bruno Cerecetto <bruno.cerecetto@gmail.com>
2020-08-10 16:39:45 -0300

    :construction: adding setup config

--- Startup.cs
+++ Startup.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -13,39 +15,78 @@ using Microsoft.Extensions.Logging;
 
 namespace WebSocketAndNetcore.Web
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddControllers();
+      services.AddSpaStaticFiles(config => config.RootPath = "wwwroot");
+      services.AddSingleton(typeof(SquareService), new SquareService());
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseStaticFiles();
+      app.UseSpaStaticFiles();
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+
+      app.UseWebSockets();
+      app.Use(async (context, next) =>
+      {
+        if (context.Request.Path == "/ws")
+        {
+          if (context.WebSockets.IsWebSocketRequest)
+          {
+            var socket = await context.WebSockets.AcceptWebSocketAsync();
+            var squareService = (SquareService)app.ApplicationServices.GetService(typeof(SquareService));
+            await squareService.AddUser(socket);
+            while (socket.State == WebSocketState.Open)
+            {
+              await Task.Delay(TimeSpan.FromMinutes(1));
+            }
+          }
+          else
+          {
+            context.Response.StatusCode = 400;
+          }
+        }
+        else
+        {
+          await next();
+        };
+      });
+      app.UseSpa(config =>
+      {
+        config.Options.SourcePath = "client-app";
+        if (env.IsDevelopment())
+        {
+          config.UseAngularCliServer("start");
+        }
+      });
+    }
+  }
 }
