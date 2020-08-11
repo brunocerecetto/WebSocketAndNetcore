da294861e5f8bc30c43638c7188270a4320113d1
Bruno Cerecetto <bruno.cerecetto@gmail.com>
2020-08-11 10:05:30 -0300

    :sparkles: adding websocket service logic

--- client-app/src/app/web-socket.service.ts
+++ client-app/src/app/web-socket.service.ts
@@ -1,9 +1,55 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Square } from './models/square';
+import { SocketMessage } from './models/socket-message';
+import { SquareChangeRequest } from './models/square-change-request';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebSocketService {
+  private socket: WebSocket;
+  private name: string;
+
+  squares$: BehaviorSubject<Square[]> = new BehaviorSubject<[]>([]);
+  announcement$: BehaviorSubject<string> = new BehaviorSubject<string>('');
+  name$: BehaviorSubject<string> = new BehaviorSubject<string>('');
 
   constructor() { }
+
+  startSocket() {
+    this.socket = new WebSocket('wss://localhost:5001/ws');
+
+    this.socket.addEventListener("open", ev => {
+      console.log('opened');
+    });
+
+    this.socket.addEventListener('message', ev => {
+      const messageBox: SocketMessage = JSON.parse(ev.data);
+
+      console.log('message object', messageBox);
+
+      switch (messageBox.MessageType) {
+        case "name":
+          this.name = messageBox.Payload;
+          this.name$.next(this.name);
+          break;
+        case "announce":
+          this.announcement$.next(messageBox.Payload);
+          break;
+        case "squares":
+          this.squares$.next(messageBox.Payload);
+          break;
+        default:
+          break;
+      }
+    })
+  }
+
+  sendSquareChangeRequest(req: SquareChangeRequest) {
+    req.Name = this.name;
+    var requestAsJson = JSON.stringify(req);
+    this.socket.send(requestAsJson);
+  }
+
 }
