89c3500661c044e5155a670d9af669f40a8b2ec1
Bruno Cerecetto <bruno.cerecetto@gmail.com>
2020-08-11 14:39:27 -0300

    :lipstick: adding html and suscription logic

--- client-app/src/app/app.component.ts
+++ client-app/src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Square } from './models';
+import { WebSocketService } from './web-socket.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client-app';
+
+  announcementSub;
+  messages: string[] = [];
+  squares: Square[] = [];
+  colors: string[] = ["red", "green", "blue"];
+  currentColor: string = "red";
+  name: string = "";
+
+  constructor(private socketService: WebSocketService) {
+    this.socketService.announcement$.subscribe(announcement => {
+      if (announcement) {
+        this.messages.unshift(announcement);
+      }
+    });
+    this.socketService.squares$.subscribe(sq => {
+      this.squares = sq;
+    });
+    this.socketService.name$.subscribe(n => {
+      this.name = n;
+    });
+  }
+
+  ngOnInit() {
+    this.socketService.startSocket();
+  }
+
+  squareClick(event, square: Square) {
+    if (square.Color === this.currentColor)
+      return;
+    var req = new SquareChangeRequest();
+    req.Id = square.Id;
+    req.Color = this.currentColor;
+    this.socketService.sendSquareChangeRequest(req);
+  }
 }
